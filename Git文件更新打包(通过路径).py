2088e43bbd1e0706b8f6164b6b7a0c76dc507638
18813158233@163.com <Cs5816497!>
2020-08-27 09:27:20 +0800

    更新了两个Git文件更新打包的代码

---
+++ Git文件更新打包(通过路径).py
@@ -0,0 +1,176 @@
+#=========================================================================
+PPAP='''文件更新检查打包.py
+创作者：ZB
+更新时间：2020/8/26
+使用注意：代码目前实现了自动更新GIT然后按照目录查找文件进行打包。
+注意：目前需要自己删除上次的打包记录'''
+
+SOUR_PATH=r'''vi_api\app\Http\Lib\AppIdManager.php
+vi_api\app\Http\Lib\ConstUtil.php'''
+#=========================================================================
+#库文件定义
+import os
+import sys
+import shutil
+import zipfile
+from git import Git
+#=========================================================================
+
+#这里是目录的定义位置
+#路径定义简单化
+#=========================================================================
+#这里是打包文件库名字设定
+NAME='Video'#一个是Video，一个是Wyy
+
+##这里是判断GIT库文件
+#=========================================================================
+if NAME=='Video':
+    EXCEL_PATH = r'C:\Users\Administrator\Desktop\1\MM-VideoInquiry.xls'  # 作为记录存储的EXCEL所在路径
+    O_PATH = r'F:\GIT\MM-VideoInquiry'  # 作为此次更新的仓库最高层目录
+else:
+    if NAME=='Wyy':
+        EXCEL_PATH = r'C:\Users\Administrator\Desktop\1\MM-HLWYY.xls'  # 作为记录存储的EXCEL所在路径
+        O_PATH = r'F:\GIT\MM-HLWYY'  # 作为此次更新的仓库最高层目录
+    else:
+        print('项目名字未找到，请确认！')
+        exit()
+
+#=========================================================================
+#打包文件夹路径和压缩文件夹路径，不用修改
+PAGPATH=r'C:\Users\Administrator\Desktop\Git获取'#用于压缩成zip的文件夹
+#=========================================================================
+#定义一下日志文件
+log_name='更新日志'
+log_msg='''更新日志内容。
+'''
+#=========================================================================
+# file_times_modified=time.localtime(os.path.getmtime(PATH))
+# Ctime=os.path.getmtime(PATH)#这里三天语句是用于测试时间戳转换时间
+# print(Ctime)
+#=========================================================================
+#全局变量声明。
+File_Time={}#创建一个新表单，用于读取和存储路径对应的时间戳内容
+New_File={}#创建一个新表单，用于存储什么文件进行过更新
+ChangeFiles = 0 #用于记录本次运行更新的文件数量
+#=========================================================================
+#文件拉取
+def get():
+    print("请问分支是什么？")
+    Q = input("Y/N(Y以外都为不更换，默认为Mater分支):")
+    if Q == "Y" or Q == "y":
+        CHECKOUT = input("请输入分支名字:")  # 切换分支
+        try:
+            repo = Git(O_PATH)  # 定义Git仓库地址
+            repo.execute("git checkout " + CHECKOUT)  # 切换分支
+            repo.execute("git pull")  # 拉取最新代码
+        except:
+            print("分支名字请确认正确")
+            exit()
+    else:
+        print("不更换分支")
+        repo = Git(O_PATH)  # 定义Git仓库地址
+        repo.execute("git checkout " + "master")  # 切换分支
+        repo.execute("git pull")  # 拉取最新代码
+        print("代码拉取完成")
+
+#=========================================================================
+#切片路径
+#获取所有Git文件路径
+def qiepian_git(PP):
+    p = 0
+    q = 0
+    for t in PP:
+        if t == '\n' :
+            GIT_PATH.append(Top+PP[q:p])
+            q = p+1
+        if p+1==len(PP):
+            GIT_PATH.append(Top+PP[q:p+1])
+        p += 1
+#获取打包文件路径
+def qiepian_pag(PP):
+    global PAGPATH
+    p = 0
+    q = 0
+    for t in PP:
+        if t == '\n' :
+            PAG_PATH.append(PAGPATH+'\\'+PP[q:p])
+            q = p+1
+        if p+1==len(PP):
+            PAG_PATH.append(PAGPATH+'\\'+PP[q:p+1])
+        p += 1
+#单个文件的目录获取
+def mulu_get(path):
+    p=-1
+    q=len(path)
+    str=''
+    while q+p>0:
+        if path[q+p]=='\\':
+            str=path[0:q+p+1]
+            break
+        else:
+            p-=1
+    return str
+
+#获取PAG目录地址
+def mulu(P):
+    for i in P:
+        path=mulu_get(i)
+        MULU_PATH.append(path)
+
+#Git_TOP获取
+def Git_Top():
+    global Top
+    if NAME == 'Video':
+        Top = 'F:\\GIT\\MM-VideoInquiry\\'
+    elif NAME == 'Wyy':
+        Top = 'F:\\GIT\\MM-HLWYY\\'
+    else:
+        print('库名错误，请确认名字')
+        sys.exit(0)
+
+#打包Git获取中的文件
+def make_zip(source_dir, output_filename):
+    zipf = zipfile.ZipFile(output_filename, 'w')
+    pre_len = len(os.path.dirname(source_dir))
+    for parent, dirnames, filenames in os.walk(source_dir):
+        for filename in filenames:
+            pathfile = os.path.join(parent, filename)
+            arcname = pathfile[pre_len:].strip(os.path.sep)   #相对路径
+            zipf.write(pathfile, arcname)
+    zipf.close()
+
+#全部打包文件名字表单
+def get_dabao_name():
+    for filename in os.listdir(r'C:\Users\Administrator\Desktop\Git获取'):
+        PAG.append(filename)
+#=========================================================================
+#主程序位置
+get()
+#此前已经进行过代码拉取
+GIT_PATH=[]
+PAG_PATH=[]
+MULU_PATH=[]
+PAG=[]
+#开始代码拉取
+
+Git_Top()
+qiepian_git(SOUR_PATH)
+qiepian_pag(SOUR_PATH)
+mulu(PAG_PATH)
+TIME=len(GIT_PATH)
+for i in range(TIME):
+    if os.path.exists(MULU_PATH[i]) == True:
+        shutil.copy(GIT_PATH[i], PAG_PATH[i])
+    else:
+        os.system("md " + MULU_PATH[i])
+        shutil.copy(GIT_PATH[i], PAG_PATH[i])
+#文件已经全部获取
+
+#获取打包区文件名
+get_dabao_name()
+#打包区文件打包
+for dabao in PAG:
+    make_zip(PAGPATH+'\\'+dabao,PAGPATH+'\\'+dabao+'.zip')
+print('打包完成，请在'+PAGPATH+'中查看')
+print(PPAP)
+#=========================================================================
