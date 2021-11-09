04d7c0fbab811f11e905c315139b68263b6546cd
zhoubiaobiao <18813158233@163.com>
2021-11-10 00:13:50 +0800

    '小球添加刚体'

--- Unity3d_Project/Rplling Ball/Assets/BallControl/BallControl.cs
+++ Unity3d_Project/Rplling Ball/Assets/BallControl/BallControl.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
+    Rigidbody rig;//Ball身上的碰撞控件
+
+    public float speed = 3; // 定义速度参数，同时可以在unity里面看到
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>(); // 获取Rigidbody组件
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+
+        //Debug.Log("水平："+h+"垂直："+v);
+
+        //rig.AddForce(new Vector3(h, 0, v) * speed );
+    }
+
+    private void FixedUpdate()
+    {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Debug.Log("水平："+h+"垂直："+v);
+        Debug.Log("水平：" + h + "垂直：" + v);
+
+        rig.AddForce(new Vector3(h, 0, v) * speed * 5);
     }
 }
