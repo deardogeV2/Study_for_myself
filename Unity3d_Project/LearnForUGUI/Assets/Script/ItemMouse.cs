7cadf0340ed56a8294dd5c258dadd71ead3e2c7d
zhoubiaobiao <18813158233@163.com>
2021-12-20 23:50:17 +0800

    '升级代码'

---
+++ Unity3d_Project/LearnForUGUI/Assets/Script/ItemMouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// 鼠标指针上的物品
+/// </summary>
+public class ItemMouse : MonoBehaviour {
+
+    private Image icon;
+    private Text num;
+    private ItemGrid itemFirstPick;//对第一次鼠标拾取物品的引用
+
+    public ItemGrid Grid
+    {
+        get { return itemFirstPick; }
+        set { 
+            itemFirstPick = value;
+            GameUI.Instance.isPickItem = true;
+            num.gameObject.SetActive(false);
+            if(itemFirstPick.Data.capacity>1)
+            {
+                num.text = itemFirstPick.Count.ToString();
+                num.gameObject.SetActive(true);
+            }
+            string iconPath = "file://" + Application.dataPath + "\\ItemIcon\\" + itemFirstPick.Data.icon;
+            StartCoroutine(Tools.LoadImage(iconPath, icon));
+        }
+    }
+	void Awake () {
+        icon = transform.GetComponent<Image>();
+        num = transform.Find("Num").GetComponent<Text>();
+	}
+}
---
+++ Unity3d_Project/LearnForUGUI/Assets/Script/PanelRoleBag/ItemGrid.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class ItemGrid : EventTrigger
+{
+    private Image icon;//图标
+    private Image lockFlag;//锁图标
+    private Text textNum;//显示物品数量
+
+
+
+    //--------------物品数据
+    ConfigItem data;
+    private int count;//物品数量
+
+    public int Count
+    {
+        get { return count; }
+    }
+
+    public ConfigItem Data
+    {
+        get { return data; }
+        set
+        {
+            data = value;
+            //通过配置数据去资源目录下读取图片
+            //icon.sprite = Resources.Load<Sprite>(data.icon);
+            if(data!=null)
+            {
+                string iconPath = "file://" + Application.dataPath + "\\ItemIcon\\" + data.icon;//Application.dataPath:指的是Assets文件夹所在的目录
+                StartCoroutine(Tools.LoadImage(iconPath, icon));
+            }
+            
+        }
+    }
+    //判断是否已经存储了物品
+    public bool isStored
+    {
+        get
+        {
+            return icon.gameObject.activeInHierarchy;
+        }
+    }
+    //判断格子是否被锁住
+    public bool isLocked
+    {
+        get
+        {
+            return lockFlag.gameObject.activeInHierarchy;
+        }
+    }
+    void Awake()
+    {
+        icon = transform.Find("Icon").GetComponent<Image>();
+        lockFlag = transform.Find("Lock").GetComponent<Image>();
+        textNum = transform.Find("Number").GetComponent<Text>();
+
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        //鼠标进入格子，显示格子上物品的信息
+        if (isStored && !GameUI.Instance.isPickItem)
+        {
+            GameUI.Instance.showItemInfo(data.descrip);
+        }
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        print("OnPointerDown");
+        if (!isStored)//格子里还没有存物品
+        {
+            if (GameUI.Instance.isPickItem)//鼠标上已经拾取
+            {
+                //icon.gameObject.SetActive(true);
+                storeItem(GameUI.Instance.ItemMouse.Grid.data, GameUI.Instance.ItemMouse.Grid.count);
+                GameUI.Instance.hideMousePickItem();//隐藏鼠标上的物品
+            }
+        }
+        else//格子里有物品
+        {
+            if (GameUI.Instance.isPickItem)//鼠标上已经拾取
+            {
+                exchangeItem(GameUI.Instance.ItemMouse.Grid);
+                GameUI.Instance.hideMousePickItem();
+
+            }
+            else//鼠标上没有拾取物品
+            {
+                GameUI.Instance.ItemMouse.Grid = this;
+                icon.gameObject.SetActive(false);
+                textNum.gameObject.SetActive(false);
+                GameUI.Instance.hideItemInfo();
+            }
+        }
+        //GameUI.Instance.hideItemInfo();
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        //鼠标离开格子
+        if (isStored)
+        {
+            GameUI.Instance.hideItemInfo();
+        }
+    }
+
+    /// <summary>
+    /// 往已经存放了物品的格子中追加物品
+    /// </summary>
+    /// <param name="num"></param>
+    public void addItem(int num = 1)
+    {
+        this.count += num;
+        if (textNum)
+        {
+            if (data.capacity > 1)
+            {
+                textNum.text = this.count.ToString();
+                textNum.gameObject.SetActive(true);
+            }
+            else
+            {
+                textNum.text = "";
+                //textNum.gameObject.SetActive(false);
+            }
+        }
+    }
+    /// <summary>
+    /// 存储物品
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="num"></param>
+    public void storeItem(ConfigItem data, int num = 1)
+    {
+        textNum.gameObject.SetActive(false);
+        //icon.gameObject.SetActive(true);
+        Data = data;
+        this.count = num;
+        if (count > 1)
+        {
+            textNum.text = count.ToString();
+            textNum.gameObject.SetActive(true);
+        }
+    }
+    /// <summary>
+    /// 交换两个格子中的物品
+    /// </summary>
+    /// <param name="other"></param>
+    public void exchangeItem(ItemGrid other)
+    {
+        ConfigItem otherData = other.data;
+        int otherCount = other.count;
+
+        other.storeItem(data, count);
+
+        this.storeItem(otherData, otherCount);
+    }
+
+
+}
