4680495076c3d0b52dfb1f56ea73a20e14197335
zhoubiaobiao <zhoubiaobiao@bytedance.com>
2021-12-21 11:38:28 +0800

    添加老师的测试工程，用于了解脚本的载体

---
+++ Unity3d_Project/TeacherUGUI/Assets/Scripts/PanelRoleBag/PanelRoleBag.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelRoleBag : MonoBehaviour {
+    public ItemGrid[] items;//面板上所有的背包格子
+	
+	void Start () {
+        GameObject goBag = GameObject.Find("Bag");
+        items = goBag.GetComponentsInChildren<ItemGrid>();
+        foreach (var item in items)
+        {
+            item.Data = null;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /// <summary>
+    /// 从已经存储的物品中找ID相同的物品
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    private ItemGrid findTheSameItem(int id)
+    {
+        foreach (ItemGrid s in items)
+        {
+            if (s.isStored && s.Data!=null)
+            {
+                if(s.Data.id == id)
+                {
+                    return s;
+                }
+                
+            }
+        }
+
+        return null;
+    }
+    /// <summary>
+    /// 从所有格子中查找一个空格子
+    /// </summary>
+    /// <returns></returns>
+    private ItemGrid findEmptyItem()
+    {
+        foreach (ItemGrid s in items)
+        {
+            if (!s.isStored)//没有存放数据
+            {
+                return s;
+            }
+        }
+
+        //没有找到空的物品槽，说明背包以满
+        return null;
+    }
+    /// <summary>
+    /// 存储物品到背包中
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public bool storeItemToBag(ConfigItem d)
+    {
+        if (d == null) return false;
+        if (d.capacity == 1)//如果该物品在物品槽中只能存放一个
+        {
+            ItemGrid item = findEmptyItem();//直接找到一个还没有使用的物品格子
+            if (item != null)//如果找到了空的物品槽，将该物品放进去格子里
+            {
+                item.storeItem(d);
+            }
+            else//如果没有找到，则将说明背包已满
+            {
+                Debug.Log("没有找到空的物品槽，可能是背包以满");
+                return false;
+            }
+        }
+        else if (d.capacity > 1)//物品可以叠加
+        {
+            //从背包中查找一个相同ID的物品
+            ItemGrid item = findTheSameItem(d.id);
+            if (item != null)
+            {
+                item.addItem(1);//如果找到,直接叠加一个单位
+            }
+            else
+            {
+                item = findEmptyItem();//如果没有找到已存物体,继续查找空格子
+                if (item != null)
+                {
+                    item.storeItem(d);//找到空格子,直接存放该物品
+                }
+                else
+                {
+                    Debug.Log("没有找到空的物品槽，可能是背包以满,需要扩充背包格子!!!");
+                    return false;
+                }
+            }
+        }
+        return true; ;
+    }
+}
---
+++ Unity3d_Project/TeacherUGUI/Assets/Scripts/Utility/Tools.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Tools
+{
+
+    public static Transform findChild(Transform transParent, string childName)
+    {
+        Transform transChild = transParent.Find(childName);
+        if (transChild == null)//如果没有找到
+        {
+            foreach (Transform t in transParent)//父物体下面的所有子物体
+            {
+                transChild = findChild(t, childName);
+                if (transChild != null)
+                {
+                    return transChild;
+                }
+            }
+        }
+        return transChild;
+    }
+
+    //加载图片(对2D游戏使用)
+    public static IEnumerator LoadImage(string url, SpriteRenderer render)
+    {
+        WWW www = new WWW(url);
+
+        while (!www.isDone)
+            yield return www;
+        
+        Texture2D texture = www.texture;
+        Sprite sp = Sprite.Create(
+            texture,
+            new Rect(0, 0, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f));
+        render.sprite = sp;
+    }
+    //对UI使用
+    public static IEnumerator LoadImage(string url, Image image)
+    {
+        WWW www = new WWW(url);
+
+        while (!www.isDone)
+            yield return www;
+
+        Texture2D texture = www.texture;
+        Sprite sp = Sprite.Create(
+            texture,
+            new Rect(0, 0, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f));
+        image.sprite = sp;
+        image.gameObject.SetActive(true);
+    }
+
+    public static void rotateTheTarget(Transform self, Transform target, float speedAngle = 30.0f)
+    {
+        if (target == null)
+        {
+            //self.eulerAngles = Vector3.zero;
+            return;
+        }
+
+        Vector3 pointSelf = self.position;
+        Vector3 pointAim = target.position;
+        pointSelf.y = 0.0f;
+        pointAim.y = 0.0f;
+        Vector3 dir = (pointAim - pointSelf).normalized;
+        Quaternion destRotation = Quaternion.LookRotation(dir);
+        self.rotation = Quaternion.Slerp(self.rotation, destRotation, Time.deltaTime * speedAngle);//插值运算
+    }
+}
+
